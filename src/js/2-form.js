907a9f5226c8afe35f69f3164355df6d198a30fc
Nikitin <161200278+Nikitincv@users.noreply.github.com>
2024-08-06 16:51:54 +0200

    1

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+
+const form = document.querySelector(".feedback-form");
+const LS_KEY = "feedback-form-state";
+
+const {email, message} = form.elements
+
+const formData = JSON.parse(localStorage.getItem(LS_KEY)) || {
+    email: "",
+     message: ""
+}
+
+email.value = formData.email;
+message.value = formData.message;
+
+form.addEventListener("input", onInput)
+
+function onInput (evt) {
+formData[evt.target.name] = evt.target.value;
+
+
+localStorage.setItem(LS_KEY, JSON.stringify(formData))
+}
+
+form.addEventListener("submit", sendForm);
+
+function sendForm (evt) {
+    evt.preventDefault();
+    const emailValue = email.value.trim();
+    const messageValue = message.value.trim();
+    if (emailValue === "" || messageValue === "") {
+        return alert("Fill please all fields");
+    } 
+    console.log(formData);
+    localStorage.removeItem(LS_KEY);
+    form.reset();
+    formData.email = "";
+    formData.message = "";
+}
